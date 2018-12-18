5a1144fe0a784b603a5d0c2307f64693e4818f24
Unknown <dylanadriansoler@gmail.com>
2018-12-18 02:08:05 +0100

    Progesando

--- FormularioSeguro/ControlLogin.php
+++ FormularioSeguro/ControlLogin.php
@@ -6,20 +6,23 @@ require_once "GestionUsuarios.php";
 $usuario = $_POST["user"];
 $passw = $_POST["password"];
 
+$gest = new GestionUsuarios();
+
 //Comprobar si esta registrado
     //consulta usuario
+    $existe = $gest->consultarUsuario($usuario);
 
 //Si ya esta registrado
+if ($existe) {
     //Verificar contraseña
+    echo'hola k ase';
+}
 //Si no
-
+else {
     //Insertar en BD
-    $gest = new GestionUsuarios();
     $result = $gest->insertarUsuario($usuario,$passw);
 
-
-    if($result)
-    {
+    if($result) {
         echo 'Registrado';
-        echo "Usuario: ".$usu.", Contrasenia hash: ".$psw;
     }
+}
--- FormularioSeguro/GestionUsuarios.php
+++ FormularioSeguro/GestionUsuarios.php
@@ -18,17 +18,47 @@ class gestionUsuarios
 
         $passw = password_hash($psw,PASSWORD_BCRYPT);
 
-        $stmt = $conexion->prepare("INSERT INTO Usuarios(user,password) VALUES(?,?)");
+        $stmt = $conexion->prepare("INSERT INTO Usuarios(UserName,Password) VALUES(?,?)");
         $stmt->bind_param('ss',$usu,$psw);
 
         $usu = $user;
         $psw = $passw;
 
         $result = $stmt->execute();
+        $stmt->free_result();
         $stmt->close();
         $conBD->closeConnection();
 
         return $result;
     }
+
+    function  consultarUsuario($user){
+
+        $ret = false;
+
+        $conBD = ConexionBD::getInstance();
+        $conexion = $conBD->getConnection();
+
+        $stmt = $conexion->prepare("SELECT * FROM Usuarios WHERE UserName=?");
+        $stmt->bind_param('s',$user);
+        $stmt->store_result();
+
+
+
+        $stmt->execute();
+
+        if($stmt->affected_rows>0)
+        {
+            $ret = true;
+        }
+        $stmt->free_result();
+        $stmt->close();
+        $conBD->closeConnection();
+
+        return $ret;
+
+    }
+
+
 
 }
